9b8418d1bd747c4116d58e4a4fc25c2c17e3ef5e
JannickNijholt <info@jannicknijholt.nl>
2023-06-01 14:12:51 +0200

    Update mailchimp-import-v2.py
    Instead of creating a .txt file with the logs a folder is made with a ok.csv file and failed.csv file. This way it's easier to find out which contacts have been imported succesfully and which ones have failed and why.

--- mailchimp-import-v2.py
+++ mailchimp-import-v2.py
@@ -3,28 +3,50 @@ import requests
 import os
 import json
 from datetime import datetime
+import sys
 
-# Settings
-api_key = "xxxxxxxxxxxxxxxxxxxxxxx-xxxxx"
-audience_id = "xxxxxxxxxxxx"
-server_prefix = "xxxxx"
+# Settings (adjust these as needed)
+api_key = "XXXXXXXXX"
+audience_id = "XXXXXXXX"
+server_prefix = "XXXXX"
 csv_file_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), "emails.csv")
 current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-log_file_location = os.path.join(os.path.dirname(os.path.realpath(__file__)), f"mailchimp-import-log_{audience_id}_{current_datetime}.txt")
 
-# URL to add subscribers to the Mailchimp audience
+# Create a new folder for the audience id, date and time
+folder_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), f"{audience_id}_{current_datetime}")
+os.makedirs(folder_path, exist_ok=True)
+
+# Create file paths for ok.csv and failed.csv within the new folder
+ok_file_location = os.path.join(folder_path, "ok.csv")
+failed_file_location = os.path.join(folder_path, "failed.csv")
+
+# URL for adding members to a Mailchimp list
 url = f"https://{server_prefix}.api.mailchimp.com/3.0/lists/{audience_id}/members"
 
-# Headers for the API-request
+# Headers for the API request
 headers = {
     "Content-Type": "application/json",
     "Authorization": f"apikey {api_key}"
 }
 
-# Read emaialdressses and the full name from the CSV-file and add them as a subscriber to the Mailchimp audience.
-with open(csv_file_location, "r", encoding="utf-8") as file, open(log_file_location, "a") as log_file:
+# Read the total number of contacts
+with open(csv_file_location, "r", encoding="utf-8") as file:
+    total_contacts = sum(1 for row in csv.reader(file)) - 1  # -1 for the header
+
+# Read email addresses and full names from a CSV file and add them as a member of the Mailchimp list
+with open(csv_file_location, "r", encoding="utf-8") as file, \
+     open(ok_file_location, "w", newline='', encoding="utf-8") as ok_file, \
+     open(failed_file_location, "w", newline='', encoding="utf-8") as failed_file:
+    
     reader = csv.reader(file)
-    next(reader)  # Skip header row
+    ok_writer = csv.writer(ok_file)
+    failed_writer = csv.writer(failed_file)
+
+    next(reader)  # skip header row
+    
+    contacts_imported = 0
+    contacts_failed = 0
+
     for row in reader:
         first_name, last_name, email = row
         data = {
@@ -39,8 +61,16 @@ with open(csv_file_location, "r", encoding="utf-8") as file, open(log_file_locat
         response_data = json.loads(response.text)
 
         if response.status_code == 200:  # Successful subscription
-            log_message = f"{email} - {response_data['status']}\n"
+            ok_writer.writerow([first_name, last_name, email])
+            contacts_imported += 1
         else:  # Any other status
-            log_message = json.dumps(response_data, indent=2) + "\n"
+            failed_writer.writerow([first_name, last_name, email, json.dumps(response_data, indent=2)])
+            contacts_failed += 1
 
-        log_file.write(log_message)
+        # Display import progress
+        contacts_remaining = total_contacts - contacts_imported - contacts_failed
+        sys.stdout.write("\rContacts remaining: {0} ({1}%), Imported: {2} ({3}%), Failed: {4} ({5}%)".format(
+            contacts_remaining, contacts_remaining / total_contacts * 100,
+            contacts_imported, contacts_imported / total_contacts * 100,
+            contacts_failed, contacts_failed / total_contacts * 100))
+        sys.stdout.flush()
